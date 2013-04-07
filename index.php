dd6126c2e71cb637248ee641ceb62ba94b7e42e2
Alan Coleman <boomfelled@gmail.com>
2013-04-07 22:33:32 +0100

    Code tidy

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
-<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if IE 8]> 				 
+<html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js" lang="en"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width" />
@@ -78,9 +80,7 @@ function dbconnect()
 	# LOCAL
 	$username 			= "root";
 	$password 			= "";
-	$database 			= "pin"; 
-
-	
+	$database 			= "pin";
 	$hostname 			= "localhost";
 	try {   
 		# MySQL with PDO_MYSQL  
